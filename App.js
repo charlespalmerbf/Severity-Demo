fcf61e84baf344b988559a258ef6635f5b168eca
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2022-03-29 16:06:20 +0100

    feat: initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import WorldMap from './WorldMap'
+
+export default function App() {
+  return (
+    <View style={{flex:1}}>
+      <WorldMap onSelectContinent={(el) => alert(el)} color='black'  selectedColor="red" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
